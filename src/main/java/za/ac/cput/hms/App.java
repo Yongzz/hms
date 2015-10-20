5d1c206b09bff2af473ecc8d127d5e12c4c6a6e8
Yongzz <yongz.ngwenduna@gmail.com>
2015-10-20 20:32:51 +0200

    first commit

---
+++ src/main/java/za/ac/cput/hms/App.java
@@ -0,0 +1,29 @@
+package za.ac.cput.hms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+public class App{
+
+    @Service
+    public static class MyService {
+        public String sampleService() {
+            return "Application Started";
+        }
+    }
+    public static void main( String[] args ){
+        SpringApplication.run(App.class, args);
+    }
+}
+
