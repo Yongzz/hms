84caabe8827523e92e13e864b12171ed194ce098
Tiego3 <tiego3m@gmail.com>
2015-10-24 12:20:26 +0200

    Added Assign, Report and Student services

---
+++ src/main/java/za/ac/cput/hms/services/report/ReportService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.hms.services.report;
+
+/**
+ * Created by student on 2015/10/24.
+ */
+public interface ReportService {
+}
---
+++ src/main/java/za/ac/cput/hms/services/student/StudentService.java
@@ -0,0 +1,7 @@
+package za.ac.cput.hms.services.student;
+
+/**
+ * Created by student on 2015/10/24.
+ */
+public interface StudentService {
+}
